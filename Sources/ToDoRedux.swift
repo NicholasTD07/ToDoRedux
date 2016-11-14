41ff52e09e76c089f639b46881069db3a5832e21
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-14 17:08:50 +1100

    Can add ToDo to a State (which has todos of type [ToDo])

--- Sources/ToDoRedux.swift
+++ Sources/ToDoRedux.swift
@@ -1,3 +1,38 @@
 import TDRedux
 
+public typealias Store = TDRedux.Store<State>
 
+public protocol ToDoAction { }
+
+public struct ToDo {
+    public let title: String
+
+    public init(title: String) {
+        self.title = title
+    }
+}
+
+public struct State {
+    public static let initial = State.init(todos: [])
+
+    public let todos: [ToDo]
+
+    public init(todos: [ToDo]) {
+        self.todos = todos
+    }
+
+    func add(title: String) -> State {
+        return .init(todos: todos + [.init(title: title)])
+    }
+}
+
+public enum ToDoActions: ToDoAction {
+    case add(title: String)
+}
+
+public let reducer = Reducer(initialState: State.initial) { (state, action: ToDoActions) in
+    switch action {
+    case let .add(title):
+        return state.add(title: title)
+    }
+}
