a4255a1b08d17eb558ee99d56489dda83e0e5644
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-15 22:30:09 +1100

    Add ToDos

--- Sources/Models.swift
+++ Sources/Models.swift
@@ -3,6 +3,13 @@ import Foundation
 public typealias Id = UUID
 public typealias Date = Foundation.Date
 
+public struct ToDos {
+    public let inbox: [ToDo] // default
+    public let today: [ToDo] // get these done today
+    public let later: [ToDo] // no plan for these yet
+    public let schedueld: [ToDo] // has due dates
+}
+
 public struct Tag {
     public let name: String
 }
