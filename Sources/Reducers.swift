4254e37f90b5b50d375f87a8e7d7842a0dfee41f
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-15 16:57:09 +1100

    Move Reducers into its own file

---
+++ Sources/Reducers.swift
@@ -0,0 +1,49 @@
+import TDRedux
+
+public let reducer = TDRedux.combine(reducers: [
+    tagReducer,
+    todoReducer,
+])
+
+public let tagReducer = Reducer(initialState: State.initial) { (state, action: TagActions) in
+    switch action {
+    case let .add(name):
+        return state.addTag(name: name)
+    }
+}
+
+// MARK: - Tags related actions
+extension State {
+    fileprivate func addTag(name: String) -> State {
+        return .init(
+            tags: tags + [.init(name: name)],
+            todos: todos
+        )
+    }
+}
+
+public let todoReducer = Reducer(initialState: State.initial) { (state, action: ToDoActions) in
+    switch action {
+    case let .add(title, notes, tags):
+        return state.addToDo(title: title, notes: notes, tags: tags)
+    case let .remove(todo):
+        return state.remove(todo: todo)
+    }
+}
+
+// MARK: - ToDos related actions
+extension State {
+    fileprivate func addToDo(title: String, notes: String, tags: [Tag]) -> State {
+        return .init(
+            tags: self.tags,
+            todos: todos + [.init(title: title, notes: notes, tags: tags)]
+        )
+    }
+
+    fileprivate func remove(todo: ToDo) -> State {
+        return .init(
+            tags: tags,
+            todos: todos.filter { $0.id != todo.id }
+        )
+    }
+}
--- Sources/ToDoRedux.swift
+++ Sources/ToDoRedux.swift
@@ -16,34 +16,6 @@ public struct State {
     }
 }
 
-// MARK: - ToDos related actions
-extension State {
-    fileprivate func addToDo(title: String, notes: String, tags: [Tag]) -> State {
-        return .init(
-            tags: self.tags,
-            todos: todos + [.init(title: title, notes: notes, tags: tags)]
-        )
-    }
-
-    fileprivate func remove(todo: ToDo) -> State {
-        return .init(
-            tags: tags,
-            todos: todos.filter { $0.id != todo.id }
-        )
-    }
-}
-
-
-// MARK: - Tags related actions
-extension State {
-    fileprivate func addTag(name: String) -> State {
-        return .init(
-            tags: tags + [.init(name: name)],
-            todos: todos
-        )
-    }
-}
-
 public enum ToDoActions: ToDoAction {
     case add(title: String, notes: String, tags: [Tag])
     case remove(todo: ToDo)
@@ -51,25 +23,4 @@ public enum ToDoActions: ToDoAction {
 
 public enum TagActions: ToDoAction {
     case add(name: String)
-}
-
-public let reducer = TDRedux.combine(reducers: [
-    tagReducer,
-    todoReducer,
-])
-
-public let tagReducer = Reducer(initialState: State.initial) { (state, action: TagActions) in
-    switch action {
-    case let .add(name):
-        return state.addTag(name: name)
-    }
-}
-
-public let todoReducer = Reducer(initialState: State.initial) { (state, action: ToDoActions) in
-    switch action {
-    case let .add(title, notes, tags):
-        return state.addToDo(title: title, notes: notes, tags: tags)
-    case let .remove(todo):
-        return state.remove(todo: todo)
-    }
 }
