be113da5649c36b730715709795127e0f49d8c1a
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-15 12:14:58 +1100

    Add notes in ToDo

--- Sources/ToDoRedux.swift
+++ Sources/ToDoRedux.swift
@@ -6,9 +6,11 @@ public protocol ToDoAction: TDRedux.Action { }
 
 public struct ToDo {
     public let title: String
+    public let notes: String
 
-    public init(title: String) {
+    public init(title: String, notes: String = "") {
         self.title = title
+        self.notes = notes
     }
 }
 
