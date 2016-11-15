a38fc6e5bae1f84ff85ebca7e9c8e597ef98e80f
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-15 22:28:45 +1100

    Move ToDo into its own file

--- Sources/Models.swift
+++ Sources/Models.swift
@@ -3,32 +3,6 @@ import Foundation
 public typealias Id = UUID
 public typealias Date = Foundation.Date
 
-public struct ToDo {
-    internal let id: Id
-    public let title: String
-    public let done: Bool
-    public let due: Date?
-    public let notes: String
-    public let tags: [Tag]
-
-    internal init(
-        id: Id = Id(),
-        title: String,
-        done: Bool = false,
-        due: Date?,
-        notes: String,
-        tags: [Tag]
-    ) {
-        self.id = id
-        self.title = title
-        self.done = done
-        self.due = due
-        self.notes = notes
-        self.tags = tags
-    }
-
-}
-
 public struct Tag {
     public let name: String
 }
---
+++ Sources/ToDo.swift
@@ -0,0 +1,25 @@
+public struct ToDo {
+    internal let id: Id
+    public let title: String
+    public let done: Bool
+    public let due: Date?
+    public let notes: String
+    public let tags: [Tag]
+
+    internal init(
+        id: Id = Id(),
+        title: String,
+        done: Bool = false,
+        due: Date?,
+        notes: String,
+        tags: [Tag]
+    ) {
+        self.id = id
+        self.title = title
+        self.done = done
+        self.due = due
+        self.notes = notes
+        self.tags = tags
+    }
+
+}
