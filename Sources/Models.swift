b507e48006d77df4c4ad981bf6841a5dc26cd0c6
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-15 14:03:55 +1100

    Add a model: Tag

--- Sources/Models.swift
+++ Sources/Models.swift
@@ -1,3 +1,19 @@
+import Foundation
+
+public typealias Id = UUID
+
+public struct Tag {
+    public let id: Id
+    public let name: String
+
+    public init(name: String) {
+        self.name = name
+
+        self.id = Id()
+    }
+}
+
+
 public struct ToDo {
     public let title: String
     public let notes: String
