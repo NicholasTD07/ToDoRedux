2a1b3c639059eef7291e3a8d7519af2cc9e9f00f
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-15 14:08:08 +1100

    Add tags to State

--- Sources/ToDoRedux.swift
+++ Sources/ToDoRedux.swift
@@ -5,16 +5,21 @@ public typealias Store = TDRedux.Store<State>
 public protocol ToDoAction: TDRedux.Action { }
 
 public struct State {
-    public static let initial = State.init(todos: [])
+    public static let initial = State.init(tags: [], todos: [])
 
+    public let tags: [Tag]
     public let todos: [ToDo]
 
-    public init(todos: [ToDo]) {
+    public init(tags: [Tag], todos: [ToDo]) {
+        self.tags = tags
         self.todos = todos
     }
 
     func add(title: String, notes: String, tags: [Tag]) -> State {
-        return .init(todos: todos + [.init(title: title, notes: notes, tags: tags)])
+        return .init(
+            tags: self.tags,
+            todos: todos + [.init(title: title, notes: notes, tags: tags)]
+        )
     }
 }
 
