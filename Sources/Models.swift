d00ea1295942cd5c6a6fad92e92f0326114b5a8f
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-16 11:37:58 +1100

    Make `ToDos.schedueld` a computed var

--- Sources/Models.swift
+++ Sources/Models.swift
@@ -7,7 +7,10 @@ public struct ToDos {
     public let inbox: [ToDo] // default
     public let today: [ToDo] // get these done today
     public let later: [ToDo] // no plan for these yet
-    public let schedueld: [ToDo] // has due dates
+
+    public var schedueld: [ToDo] { // has due dates
+        return all.filter { $0.due != nil }
+    }
 }
 
 public struct Tag {
