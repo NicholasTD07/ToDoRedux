53b1f66ceac49c5371a717b0eca4c9543724c1a2
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-17 23:46:29 +1100

    Move ToDos into its own file

--- Sources/ToDoRedux/Models.swift
+++ Sources/ToDoRedux/Models.swift
@@ -3,51 +3,6 @@ import Foundation
 public typealias Id = UUID
 public typealias Date = Foundation.Date
 
-public struct ToDos {
-    public static let initial = ToDos.init(todos: [])
-
-    public enum Box {
-        case inbox
-        case today
-        case later
-    }
-
-    internal struct ToDoInBox {
-        let todo: ToDo
-        let box: Box
-    }
-
-    internal let todos: [ToDoInBox]
-}
-
-extension ToDos {
-    public var inbox: [ToDo] {
-        return todos
-            .filter { $0.box == .inbox }
-            .map { $0.todo }
-    }
-    public var today: [ToDo] {
-        return todos
-            .filter { $0.box == .today }
-            .map { $0.todo }
-    }
-    public var later: [ToDo] {
-        return todos
-            .filter { $0.box == .later }
-            .map { $0.todo }
-    }
-
-    public var all: [ToDo] {
-        return todos.map { $0.todo }
-    }
-    public var done: [ToDo] {
-        return all.filter { $0.done }
-    }
-    public var schedueld: [ToDo] {
-        return all.filter { $0.due != nil }
-    }
-}
-
 public struct Tag {
     public let name: String
 }
