51a61153c45eb22de43ab5008e7c743d00ada610
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-17 19:44:02 +1100

    Update App to use store and actions

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -1,5 +1,14 @@
 import ToDoRedux
 
 internal struct App {
-    let todos = ToDos()
+    let store = Store.init(with: ToDoRedux.reducer)
+    var todos: ToDos { return store.state.todos }
+
+    func add(_ todo: ToDo, to box: ToDos.Box) {
+        store.dispatch(ToDoActions.add(todo: todo, to: box))
+    }
+
+    func done(_ todo: ToDo) {
+        store.dispatch(ToDoActions.done(todo: todo))
+    }
 }
--- Sources/App/ToDoReduxExtensions.swift
+++ Sources/App/ToDoReduxExtensions.swift
@@ -1,10 +1,1 @@
 import ToDoRedux
-
-extension ToDo {
-    static let test = ToDo(
-        title: "test",
-        due: nil,
-        notes: "",
-        tags: []
-    )
-}
